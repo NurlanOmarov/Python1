42f01d2dfee4319b81cfae3a4339dcf28f4e861c
Nurlan <omarov.nb@gmail.com>
2022-03-21 18:21:06 +0600

    Задание 1_3

--- lesson_1/DZ_1_3.py
+++ lesson_1/DZ_1_3.py
@@ -1,12 +1,13 @@
 # Зададча 1.3
 # Реализовать склонение слова «процент» во фразе «N процентов». Вывести эту фразу на экран отдельной строкой для каждого из чисел в интервале от 1 до 100:
 
-percent = int(input('Введите число процента: '))
-if percent == 11:
-    print(percent, 'процентов')
-elif percent % 10 == 1:
-    print(percent, 'процент')
-elif percent % 10 == 2 or percent % 10 == 3 or percent % 10 == 4:
-    print(percent, 'процента')
-else:
-    print(percent, 'процентов')
+percent = int()
+for percent in range (1, 101):
+    if percent == 11:
+        print(percent, 'процентов')
+    elif percent % 10 == 1:
+        print(percent, 'процент')
+    elif percent % 10 == 2 or percent % 10 == 3 or percent % 10 == 4:
+        print(percent, 'процента')
+    else:
+        print(percent, 'процентов')
